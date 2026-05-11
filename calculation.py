a8a37d54fe41f887a268c4fd9bb6eeb070e7f63a
SiriyakornThintawee <siriyakorn.thintawee@gmail.com>
2026-05-11 14:23:58 +0700

    Update calculation.py

--- calculation.py
+++ calculation.py
@@ -1,10 +1,10 @@
+from data_regulation import far_osr_data
 class calculatorareas:
     def __init__(self, project_create):
         self.name = project_create['name']
         self.area = float(project_create['areas'])
         self.sub_zoning = project_create['sub_zoning']
 
-        from data_regulation import far_osr_data
         regulation = far_osr_data.get(self.sub_zoning, {"far": 0, "osr": 0})
         self.far = regulation['far']
         self.osr = regulation['osr']
@@ -18,7 +18,7 @@ class calculatorareas:
         else:
             return result
     
-    def calculate_osr(self, bonus = False):
+    def calculate_osr(self):
         return self.area * (self.osr/100)
 
     
